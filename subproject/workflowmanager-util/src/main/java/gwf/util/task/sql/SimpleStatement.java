b7136859f39f3a9501987298c7134edc2bb12ca2
DomHi <49837942+DomHi@users.noreply.github.com>
2020-06-28 15:23:10 +0200

    use define and bind in SimpleStatement

--- subproject/workflowmanager-util/src/main/java/gwf/util/task/sql/SimpleStatement.java
+++ subproject/workflowmanager-util/src/main/java/gwf/util/task/sql/SimpleStatement.java
@@ -1,6 +1,7 @@
 package gwf.util.task.sql;
 
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Update;
 
 public class SimpleStatement extends AbstractJdbiStatement {
 
@@ -13,7 +14,12 @@ public class SimpleStatement extends AbstractJdbiStatement {
 	@Override
 	public void execute(Jdbi jdbi) {
 		jdbi.useHandle(
-				handle -> handle.createUpdate(stmt).execute()
+				handle -> {
+					Update u = handle.createUpdate(stmt);
+					getDefine().forEach(u::define);
+					getBind().forEach(u::bind);
+					u.execute();
+				}
 		);
 	}
 }
