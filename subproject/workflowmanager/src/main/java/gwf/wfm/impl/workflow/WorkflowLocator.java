b19826530f9cfcb98e2eb9b2044d0d24f3bfe6f6
DomHi <49837942+DomHi@users.noreply.github.com>
2020-07-12 10:41:53 +0200

    switch to java ee api 7.0

--- subproject/workflowmanager/src/main/java/gwf/wfm/impl/workflow/WorkflowLocator.java
+++ subproject/workflowmanager/src/main/java/gwf/wfm/impl/workflow/WorkflowLocator.java
@@ -5,22 +5,24 @@ import gwf.api.discovery.WorkflowDiscovery;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import java.net.URI;
-import java.util.Objects;
 import java.util.Optional;
 
 public class WorkflowLocator {
 
-    @Inject
-    private Instance<WorkflowDiscovery> discoveries;
+	@Inject
+	private Instance<WorkflowDiscovery> discoveries;
 
-    public Optional<URI> discover() {
-        return discoveries.stream()
-                .map(WorkflowDiscovery::find)
-                .filter(Objects::nonNull)
-                .findFirst();
-    }
+	public Optional<URI> discover() {
+		for (WorkflowDiscovery d : discoveries) {
+			URI wf = d.find();
+			if (wf != null) {
+				return Optional.of(wf);
+			}
+		}
+		return Optional.empty();
+	}
 
-    public URI relative(URI current, String path) {
-        return current.resolve(path);
-    }
+	public URI relative(URI current, String path) {
+		return current.resolve(path);
+	}
 }
