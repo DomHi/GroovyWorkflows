90cd9642a1eb1420ee6df2ddc11ffa1d5d1e3a56
DomHi <49837942+DomHi@users.noreply.github.com>
2021-01-29 15:54:43 +0100

    misc changes
    Signed-off-by: DomHi <49837942+DomHi@users.noreply.github.com>

--- subproject/workflows/src/main/groovy/gwf/workflows/WorkflowsApplication.groovy
+++ subproject/workflows/src/main/groovy/gwf/workflows/WorkflowsApplication.groovy
@@ -2,11 +2,21 @@ package gwf.workflows
 
 import groovy.util.logging.Slf4j
 
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
 @Slf4j
 class WorkflowsApplication {
 
     static void main(String[] args) {
         log.info("main called")
-        new StartWorkflow().start("myTest")
+
+        Instant before = Instant.now();
+
+        try {
+            new StartWorkflow().start("myTest")
+        } finally {
+            log.info("Execution took: {} ms", ChronoUnit.MILLIS.between(before, Instant.now()))
+        }
     }
 }
